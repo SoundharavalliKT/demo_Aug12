609fdc746690750e84937c31e17465df0f3a6245
SoundharavalliKT <soundharyakathiresan@gmail.com>
2022-08-12 12:35:25 +0530

    Updation by new=branch-1

--- file2.py
+++ file2.py
@@ -1,3 +1,3 @@
 print("This is Soundh")
 print("Newly added line after first commit")
-print("Change in new-branch-1 ## This change is reflected in file2.py in new-branch-1 but not in file2.py in master branch")
+print("Updation test : Change in new-branch-1 ## This change is reflected in file2.py in new-branch-1 but not in file2.py in master branch")
